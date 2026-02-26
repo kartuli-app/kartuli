1092ed82c35c20e78c5cf1c63e96360535a719c7
rocescoca <vrocescoca@gmail.com>
2026-02-26 19:18:43 +0400

    feat(game-client): add option to disable Serwist in development mode

--- apps/game-client/src/app/serwist-provider.tsx
+++ apps/game-client/src/app/serwist-provider.tsx
@@ -3,5 +3,9 @@
 import { SerwistProvider } from '@serwist/turbopack/react';
 
 export function SerwistProviderWrapper({ children }: { children: React.ReactNode }) {
-  return <SerwistProvider swUrl="/serwist/sw.js">{children}</SerwistProvider>;
+  return (
+    <SerwistProvider swUrl="/serwist/sw.js" disable={process.env.NODE_ENV === 'development'}>
+      {children}
+    </SerwistProvider>
+  );
 }
