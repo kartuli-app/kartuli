87930ac4254249e6f20945dffe9f721a734bd148
rocescoca <vrocescoca@gmail.com>
2026-03-13 09:49:43 +0400

    refactor(game-client): simplify HomeHeading component by removing unused className prop
    - Removed the unused className prop from the HomeHeading component to streamline its interface.
    - This change enhances code clarity and reduces unnecessary complexity.

--- apps/game-client/src/screens/home/home-heading.tsx
+++ apps/game-client/src/screens/home/home-heading.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx';
 import { useTranslation } from 'react-i18next';
 
-export function HomeHeading({ className }: { className?: string }) {
+export function HomeHeading() {
   const { t } = useTranslation('common');
   return (
-    <div className={clsx('flex flex-col', 'gap-brand-regular', className)}>
+    <div className={clsx('flex flex-col', 'gap-brand-regular')}>
       <h1 className={clsx('text-5xl')}>გამარჯობა anon</h1>
       <h2 className={clsx('text-3xl font-bold')}>{t('homeHeading')}</h2>
     </div>
