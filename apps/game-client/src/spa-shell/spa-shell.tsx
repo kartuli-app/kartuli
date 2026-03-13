da54eaffb25b503239da6f445c98a131a6f769ed
rocescoca <vrocescoca@gmail.com>
2026-03-13 09:49:34 +0400

    refactor(game-client): remove unused Banner component from SpaShell
    - Eliminated the Banner component and its related code from the SpaShell to streamline the component structure.
    - This change reduces unnecessary complexity and improves maintainability of the codebase.

--- apps/game-client/src/spa-shell/spa-shell.tsx
+++ apps/game-client/src/spa-shell/spa-shell.tsx
@@ -1,9 +1,7 @@
-'use client';
 import { I18nShell } from '@game-client/i18n/I18n-shell';
 import { RouterProvider } from '@game-client/router-outlet/router-context';
 import { RouterOutlet } from '@game-client/router-outlet/router-outlet';
 import { PWANotifications } from '@game-client/service-worker/pwa-notifications/pwa-notifications';
-import { Banner, BannerMessage } from '@kartuli/ui/components/banner/banner';
 
 interface SpaShellProps {
   readonly initialPath: string;
@@ -14,14 +12,6 @@ export function SpaShell({ initialPath }: SpaShellProps) {
     <div className="flex flex-1 flex-col">
       <RouterProvider initialPath={initialPath}>
         <I18nShell>
-          <Banner
-            onDismiss={() => {}}
-            dismissLabel="Dismiss"
-            testId="test-banner"
-            actions={[{ label: 'Action test', onClick: () => {} }]}
-          >
-            <BannerMessage>Action test</BannerMessage>
-          </Banner>
           <PWANotifications />
           <RouterOutlet />
         </I18nShell>
