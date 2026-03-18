c7421d8cad7e96884f4753744d44bc83e8a3039e
rocescoca <vrocescoca@gmail.com>
2026-03-18 17:49:53 +0400

    refactor(game-client): enhance NewModulesList with student item activity tracking
    - Integrated `useStudentItemActivityEventsCollection` and `useLiveQuery` to track student item activity events.
    - Improved loading state handling to ensure proper rendering during data fetching.
    - Added functionality to log click events for item activity, enhancing user interaction tracking.

--- apps/game-client/src/screens/home/new-modules-list.tsx
+++ apps/game-client/src/screens/home/new-modules-list.tsx
@@ -1,16 +1,35 @@
 'use client';
 
+import { useStudentItemActivityEventsCollection } from '@game-client/core/learning-content/db/student/item-activity-events-collection/use-student-item-activity-events-collection';
 import { useHomeModulesView } from '@game-client/core/learning-content/db/views/home-modules-view/use-home-modules-view';
 import { useLang } from '@game-client/i18n/use-lang';
+import { useLiveQuery } from '@tanstack/react-db';
 
 export function NewModulesList() {
   const locale = useLang();
   const contentRevision = '1.0.0';
 
-  const { data, isLoading, isError } = useHomeModulesView({ locale, contentRevision });
+  const { data, isLoading } = useHomeModulesView({ locale, contentRevision });
 
-  console.info('🚀 ~ useNewModulesList ~ data:', data);
-  console.info('🚀 ~ useNewModulesList ~ isLoading:', isLoading);
-  console.info('🚀 ~ useNewModulesList ~ isError:', isError);
-  return null;
+  const studentItemActivityEventsCollection = useStudentItemActivityEventsCollection();
+  const { data: studentItemActivityEvents, isLoading: isLoadingStudentItemActivityEvents } =
+    useLiveQuery(studentItemActivityEventsCollection);
+  if (isLoadingStudentItemActivityEvents || isLoading) {
+    return null;
+  }
+  const onClick = () => {
+    studentItemActivityEventsCollection.utils.writeInsert({
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      itemId: 'letter-ani',
+      eventType: 'click',
+    });
+  };
+  console.info('🚀 ~ NewModulesList ~ data:', data);
+  console.info('🚀 ~ NewModulesList ~ studentItemActivityEvents:', studentItemActivityEvents);
+  return (
+    <button type="button" onClick={onClick}>
+      Add item activity event
+    </button>
+  );
 }
