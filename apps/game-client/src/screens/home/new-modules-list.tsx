68207e7a924c49b20d063e8c74540cd528a40748
rocescoca <vrocescoca@gmail.com>
2026-03-18 19:46:06 +0400

    chore(game-client): update dependencies and remove unused collections
    - Added `rxdb` and `rxjs` to the project dependencies for enhanced data management capabilities.
    - Integrated `@tanstack/rxdb-db-collection` for improved database interactions.
    - Removed obsolete files related to student item activity events collection to streamline the codebase.

--- apps/game-client/src/screens/home/new-modules-list.tsx
+++ apps/game-client/src/screens/home/new-modules-list.tsx
@@ -1,35 +1,18 @@
 'use client';
 
-import { useStudentItemActivityEventsCollection } from '@game-client/core/learning-content/db/student/item-activity-events-collection/use-student-item-activity-events-collection';
+import { StudentItemActivityEventsDebug } from '@game-client/core/learning-content/db/student/students-thing';
 import { useHomeModulesView } from '@game-client/core/learning-content/db/views/home-modules-view/use-home-modules-view';
 import { useLang } from '@game-client/i18n/use-lang';
-import { useLiveQuery } from '@tanstack/react-db';
 
 export function NewModulesList() {
   const locale = useLang();
   const contentRevision = '1.0.0';
 
   const { data, isLoading } = useHomeModulesView({ locale, contentRevision });
-
-  const studentItemActivityEventsCollection = useStudentItemActivityEventsCollection();
-  const { data: studentItemActivityEvents, isLoading: isLoadingStudentItemActivityEvents } =
-    useLiveQuery(studentItemActivityEventsCollection);
-  if (isLoadingStudentItemActivityEvents || isLoading) {
+  console.info('🚀 ~ NewModulesList ~ data:', data);
+  if (isLoading) {
     return null;
   }
-  const onClick = () => {
-    studentItemActivityEventsCollection.utils.writeInsert({
-      id: crypto.randomUUID(),
-      timestamp: Date.now(),
-      itemId: 'letter-ani',
-      eventType: 'click',
-    });
-  };
-  console.info('🚀 ~ NewModulesList ~ data:', data);
-  console.info('🚀 ~ NewModulesList ~ studentItemActivityEvents:', studentItemActivityEvents);
-  return (
-    <button type="button" onClick={onClick}>
-      Add item activity event
-    </button>
-  );
+
+  return <StudentItemActivityEventsDebug />;
 }
